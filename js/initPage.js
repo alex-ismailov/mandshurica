2d00e6bfeed0f6905fd5db66156256a8395511ed
Alex-Smil <alexsmile39@gmail.com>
2019-10-09 13:41:23 +0300

    Исправил bug ломающий верстку из-за размеров картинок, теперь кладка отображается корректно, для этого добавил стили css для картинок: .gallery-item-img {   width: 100%;   height: auto;   display: block; } Сделал кладку адаптивной, по сетке bootstrap 4.

--- js/initPage.js
+++ js/initPage.js
@@ -1,21 +1,43 @@
 
 
 window.onload = () => {
-    const masonry = new Macy({
+    // macyInit();
+    macyInit2();
+};
+
+function macyInit2() {
+    var macy = Macy({
         container: '.gallery',
-        mobileFirst: true,
-        columns: 1,
+        trueOrder: false,
+        waitForImages: false,
+        margin: 3,
+        columns: 4,
         breakAt: {
-            400: 2,
-            700: 3,
-            1100: 4
-        },
-        margin: {
-            x: 5,
-            y: 5
-        },
-        waitForImages: true
+            960: 3,
+            720: 2,
+            540: 1
+        }
     });
 
-    console.log('Create new Macy');
-};
+    console.log('Create new Macy by macyInit2()');
+}
+
+// function macyInit() {
+//     const masonry = new Macy({
+//         container: '.gallery',
+//         mobileFirst: true,
+//         columns: 1,
+//         breakAt: {
+//             400: 2,
+//             700: 3,
+//             1100: 4
+//         },
+//         margin: {
+//             x: 5,
+//             y: 5
+//         },
+//         waitForImages: true
+//     });
+
+//     console.log('Create new Macy by macyInit()');
+// }
